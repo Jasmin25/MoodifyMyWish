0a04e731c18a7d58ac858b56c957c459ae9a8131
Jasmin <jasmin_shah@live.com>
2023-04-21 10:57:12 +0000

    started linting and added some tests

--- app.py
+++ app.py
@@ -1,96 +1,113 @@
+"""This is a module that creates personalized messages
+based on the occasion, relation, and other details."""
+
 import os
-import openai
 import calendar
+from datetime import date
+import openai
 import pandas as pd
 import streamlit as st
 from streamlit_extras import buy_me_a_coffee
-from datetime import date
-from streamlit.components.v1 import html
 
 # Check if we're running on Heroku
-if 'DYNO' in os.environ:
+if "DYNO" in os.environ:
     # Load Heroku environment variables
-    openai.api_key = os.environ.get('OPENAI_APIKEY')
+    openai.api_key = os.environ.get("OPENAI_APIKEY")
 else:
     # Load local environment variables from .env file
     from dotenv import load_dotenv
+
     load_dotenv()
-    openai.api_key = os.getenv('OPENAI_APIKEY')
+    openai.api_key = os.getenv("OPENAI_APIKEY")
 
 # Load the celebrities.csv file
 celebrities_df = pd.read_csv("data/celebrities.csv")
 
+
 def get_options_from_file(filename):
+    """Read the options from a given file."""
     # Get the path to the data folder
     data_folder = "data"
 
     # Join the folder path and the filename
     file_path = os.path.join(data_folder, filename)
 
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         options = [line.strip() for line in file]
     return options
 
+
 def get_input(label, filename=None):
+    """Get input from the user based on the given label and filename."""
     if filename:
         options = get_options_from_file(filename)
-        options.insert(0, '')  # Add an empty option at the beginning of the list
+        options.insert(
+            0, ""
+        )  # Add an empty option at the beginning of the list
         return st.selectbox(label, options)
-    else:
-        return st.text_input(label)
+    return st.text_input(label)
 
 
-def get_celebrity_trivia(day, month):
-    celebs = celebrities_df[(celebrities_df["day"] == day) & (celebrities_df["month"] == month)].sort_values(by="birth_year", ascending=False).head(3)
+def get_celebrity_trivia(day, month, celebrities_df):
+    """Get celebrity trivia for a given day and month."""
+    celebs = (
+        celebrities_df[
+            (celebrities_df["day"] == day) & (celebrities_df["month"] == month)
+        ]
+        .sort_values(by="birth_year", ascending=False)
+        .head(3)
+    )
     trivia_list = []
     for _, celeb in celebs.iterrows():
-        status = "still alive" if celeb["alive"] else f"died in {celeb['death_year']}"
-        trivia_list.append(f"{celeb['name']} (born {celeb['birth_year']}, {status}): {celeb['trivia']}")
+        status = (
+            "still alive"
+            if celeb["alive"]
+            else f"died in {int(celeb['death_year'])}"
+        )
+        trivia_list.append(
+            f"{celeb['name']} (born {int(celeb['birth_year'])}, {status}):"
+            f" {celeb['trivia']}"
+        )
     trivia_str = "; ".join(trivia_list)
     return trivia_str
 
+
 def max_days_for_month(month, year):
+    """Return the maximum number of days for a given month and year."""
     if month == 2:
         if year is not None and calendar.isleap(year):
             return 29
-        else:
-            return 28
-    elif month in [4, 6, 9, 11]:
+        return 28
+    if month in [4, 6, 9, 11]:
         return 30
-    else:
-        return 31
+    return 31
+
 
 st.set_page_config(
     page_title="Moodify My Wish",
     page_icon="🎉",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
 )
 
-modified_streamlit_style = """
+MODIFIED_STREAMLIT_STYLE = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            # footer:before {
-            #     content:'Made with ❤️ by Jasmin & Powered by OpenAI'; 
-            #     visibility: visible;
-            #     display: block;
-            #     bottom: 0;
-            #     text-align: center;
-            #     width: 100%;
-            #     padding: 5px;
-            #     font-size: 16px;
-            # }
-        </style>
+            </style>
 """
 
-st.markdown(modified_streamlit_style, unsafe_allow_html=True)
+st.markdown(MODIFIED_STREAMLIT_STYLE, unsafe_allow_html=True)
 
-buy_me_a_coffee.button(username="jasminshah", floating=True, width=300, text="Buy me a hot chocolate")
+buy_me_a_coffee.button(
+    username="jasminshah",
+    floating=True,
+    width=300,
+    text="Buy me a hot chocolate",
+)
 
 # Use st.sidebar to place the input widgets in the left pane
 with st.sidebar:
-
     # Add an input field for selecting the occasion
     occasion = get_input("*What's the occasion?", "occasions.txt")
 
@@ -100,30 +117,53 @@ with st.sidebar:
         month_names = [calendar.month_name[i] for i in range(1, 13)]
 
         # Use st.selectbox to choose the recipient's event month (1-12) by name
-        event_month = st.selectbox("*Choose the month", month_names, index=0)
+        EVENT_MONTH = st.selectbox("*Choose the month", month_names, index=0)
 
         # Get the month number corresponding to the selected month name
-        event_month_number = month_names.index(event_month) + 1
+        EVENT_MONTH_NUMBER = month_names.index(EVENT_MONTH) + 1
 
-        event_year_label = "Wedding Year" if occasion == "Anniversary" else "Birth Year" if occasion == "Birthday" else "Event Year"
-        include_event_year = st.checkbox(f"Know their {event_year_label.lower()}?")
-        if include_event_year:
-            event_year = st.number_input(f"Enter the {event_year_label.lower()}", min_value=1900, max_value=date.today().year, value=1900, step=1, format='%d')
+        if occasion == "Anniversary":
+            EVENT_YEAR_LABEL = "Wedding Year"
+        elif occasion == "Birthday":
+            EVENT_YEAR_LABEL = "Birth Year"
         else:
-            event_year = None
+            EVENT_YEAR_LABEL = "Event Year"
 
-        max_days = max_days_for_month(event_month_number, event_year)
+        include_EVENT_YEAR = st.checkbox(
+            f"Know their {EVENT_YEAR_LABEL.lower()}?"
+        )
+        if include_EVENT_YEAR:
+            EVENT_YEAR = st.number_input(
+                f"Enter the {EVENT_YEAR_LABEL.lower()}",
+                min_value=1900,
+                max_value=date.today().year,
+                value=1900,
+                step=1,
+                format="%d",
+            )
+        else:
+            EVENT_YEAR = None
 
-        # Use st.selectbox to choose the recipient's event day based on the valid days for the selected month and year
-        event_day = st.number_input("*Pick the day", min_value=1, max_value=max_days, value=1, step=1, format='%d')
+        MAX_DAYS = max_days_for_month(EVENT_MONTH_NUMBER, EVENT_YEAR)
+
+        # Use st.selectbox to choose the recipient's event day
+        # based on the valid days for the selected month and year
+        EVENT_DAY = st.number_input(
+            "*Pick the day",
+            min_value=1,
+            max_value=MAX_DAYS,
+            value=1,
+            step=1,
+            format="%d",
+        )
     else:
-        event_month = None
-        event_day = None
-        event_year = None
+        EVENT_MONTH = None
+        EVENT_DAY = None
+        EVENT_YEAR = None
 
     emotion = get_input("*Choose the message mood:", "moods.txt")
     relation = get_input("*What's your relation to them?", "relations.txt")
-    message_size = st.selectbox("*Choose message size:", ['S', 'M', 'L'])
+    message_size = st.selectbox("*Choose message size:", ["S", "M", "L"])
 
     # Create a row with 3 columns to center the button
     col1, col2, col3 = st.columns([1, 2, 1])
@@ -146,67 +186,101 @@ with st.sidebar:
 
 # Keep the generated message display in the main area (right pane)
 if generate_button:
-    if (occasion in ["Birthday", "Anniversary", "Wedding"] and event_month and event_day) or (occasion not in ["Birthday", "Anniversary", "Wedding"]) and emotion and relation:
+    # pylint: disable=too-many-boolean-expressions
+    if (
+        (
+            occasion in ["Birthday", "Anniversary", "Wedding"]
+            and EVENT_MONTH
+            and EVENT_DAY
+        )
+        or (occasion not in ["Birthday", "Anniversary", "Wedding"])
+        and emotion
+        and relation
+    ):
         with st.spinner(f"Crafting your {occasion.lower()} message..."):
             if occasion in ["Birthday", "Anniversary"]:
-                trivia = get_celebrity_trivia(event_day, event_month_number)
-                trivia_line = f"Some trivia of the famous people that share the same {occasion.lower()} date: {trivia}."
+                TRIVIA = get_celebrity_trivia(EVENT_DAY, EVENT_MONTH_NUMBER, celebrities_df)
+                TRIVIA_LINE = (
+                    "Some TRIVIA of the famous people that share the same"
+                    f" {occasion.lower()} date: {TRIVIA}."
+                )
             else:
-                trivia_line = ""
+                TRIVIA_LINE = ""
 
-            system_message = f"You are ChatGPT, a large language model trained by OpenAI. Your task is to generate a great {occasion.lower()} message for people based on their relation, personal information and other relevant details. More focus {occasion.lower()} and less on other information."
+            system_message = (
+                "You are ChatGPT, a large language model trained by OpenAI."
+                f" Your task is to generate a great {occasion.lower()} message"
+                " for people based on their relation, personal information"
+                " and other relevant details. More focus"
+                f" {occasion.lower()} and less on other information."
+            )
 
             if occasion in ["Birthday", "Anniversary"]:
-                event_date = date(date.today().year, event_month_number, event_day)
-                event_date_line = f"{occasion} date is : {event_date.strftime('%m-%d')}."
+                event_date = date(
+                    date.today().year, EVENT_MONTH_NUMBER, EVENT_DAY
+                )
+                EVENT_DATE_LINE = (
+                    f"{occasion} date is : {event_date.strftime('%m-%d')}."
+                )
             else:
-                event_date_line = ""
+                EVENT_DATE_LINE = ""
 
-            if occasion in ["Birthday", "Anniversary"] and event_year:
-                event_age = date.today().year - event_year
-                age_line = f"Recipient is celebrating their {event_age} years of {occasion.lower()}."
+            if occasion in ["Birthday", "Anniversary"] and EVENT_YEAR:
+                event_age = date.today().year - EVENT_YEAR
+                AGE_LINE = (
+                    f"Recipient is celebrating their {event_age} years of"
+                    f" {occasion.lower()}."
+                )
             else:
-                age_line = ""
+                AGE_LINE = ""
 
-            user_message = [
+            USER_MESSAGE = [
                 f"Generate a {occasion.lower()} message for my {relation}.",
                 f"Mood of the message should be {emotion}.",
-                event_date_line,
-                trivia_line,
-                age_line,
+                EVENT_DATE_LINE,
+                TRIVIA_LINE,
+                AGE_LINE,
             ]
 
             # Add optional fields to the user message
             if name:
-                user_message.append(f"Recipient's name is {name}.")
+                USER_MESSAGE.append(f"Recipient's name is {name}.")
             if profession:
-                user_message.append(f"Recipient's profession is {profession}.")
+                USER_MESSAGE.append(f"Recipient's profession is {profession}.")
             if hobby:
-                user_message.append(f"Recipient's hobby is {hobby}.")
+                USER_MESSAGE.append(f"Recipient's hobby is {hobby}.")
             if accomplishment:
-                user_message.append(f"Recipient's recent accomplishment: {accomplishment}.")
+                USER_MESSAGE.append(
+                    f"Recipient's recent accomplishment: {accomplishment}."
+                )
             if goal:
-                user_message.append(f"Recipient's goal or ambition: {goal}.")
+                USER_MESSAGE.append(f"Recipient's goal or ambition: {goal}.")
             if anecdote:
-                user_message.append(f"Personal anecdote or memory: {anecdote}.")
+                USER_MESSAGE.append(
+                    f"Personal anecdote or memory: {anecdote}."
+                )
             if quote:
-                user_message.append(f"Recipient's favorite quote: {quote}.")
+                USER_MESSAGE.append(f"Recipient's favorite quote: {quote}.")
             if recent_event:
-                user_message.append(f"Recent event or experience: {recent_event}.")
+                USER_MESSAGE.append(
+                    f"Recent event or experience: {recent_event}."
+                )
             if challenge:
-                user_message.append(f"Challenge the recipient is facing: {challenge}.")
+                USER_MESSAGE.append(
+                    f"Challenge the recipient is facing: {challenge}."
+                )
             if message_size == "S":
-                user_message.append("Keep the message short")
+                USER_MESSAGE.append("Keep the message short")
             if message_size == "M":
-                user_message.append("Keep the message medium size")
+                USER_MESSAGE.append("Keep the message medium size")
             if message_size == "L":
-                user_message.append("Make the message long")
+                USER_MESSAGE.append("Make the message long")
 
-            user_message = " ".join(user_message)
+            USER_MESSAGE = " ".join(USER_MESSAGE)
 
             messages = [
                 {"role": "system", "content": system_message},
-                {"role": "user", "content": user_message}
+                {"role": "user", "content": USER_MESSAGE},
             ]
 
             response = openai.ChatCompletion.create(
@@ -217,7 +291,13 @@ if generate_button:
                 temperature=0.7,
             )
 
-            message = response.choices[0].message['content'].strip().lstrip(". ").rstrip()
+            message = (
+                response.choices[0]
+                .message["content"]
+                .strip()
+                .lstrip(". ")
+                .rstrip()
+            )
 
         st.write(f"💌 **Your {occasion.lower()} message:**")
         st.markdown(f"\n{message}\n")
---
+++ tests/test_app.py
@@ -0,0 +1,54 @@
+# tests/test_app.py
+import pandas as pd
+from app import max_days_for_month, get_celebrity_trivia
+
+def test_max_days_for_month():
+    # Test for February in a leap year
+    assert max_days_for_month(2, 2020) == 29
+
+    # Test for February in a non-leap year
+    assert max_days_for_month(2, 2021) == 28
+
+    # Test for months with 30 days
+    for month in [4, 6, 9, 11]:
+        assert max_days_for_month(month, 2021) == 30
+
+    # Test for months with 31 days
+    for month in [1, 3, 5, 7, 8, 10, 12]:
+        assert max_days_for_month(month, 2021) == 31
+
+
+def test_get_celebrity_trivia():
+    # Test that the correct trivia is returned for the given date
+
+    sample_data = {
+        "day": [1, 1, 1],
+        "month": [1, 1, 1],
+        "name": ["John Doe", "Jane Doe", "Test Celebrity"],
+        "birth_year": [1900, 2000, 2010],
+        "death_year": [1950, None, None],
+        "alive": [False, True, True],
+        "trivia": [
+            "Sample trivia for John Doe.",
+            "Sample trivia for Jane Doe.",
+            "Sample trivia for Test Celebrity.",
+        ],
+    }
+
+    celebrities_df = pd.DataFrame(sample_data)
+
+    trivia = get_celebrity_trivia(1, 1, celebrities_df)
+    expected_trivia = (
+        "Test Celebrity (born 2010, still alive): Sample trivia for Test Celebrity.; "
+        "Jane Doe (born 2000, still alive): Sample trivia for Jane Doe.; "
+        "John Doe (born 1900, died in 1950): Sample trivia for John Doe."
+    )
+
+    print("Expected Trivia:", expected_trivia)
+    print("Actual Trivia:", trivia)
+
+    assert trivia == expected_trivia
+
+    # Test that an empty string is returned if no celebrities are found
+    trivia = get_celebrity_trivia(31, 2, celebrities_df)  # Invalid date
+    assert trivia == ""
