f76c5a13f702864b079514dd54efc3c535ed67a6
Jasmin <jasmin_shah@live.com>
2023-04-30 11:28:35 +0000

    update score functions

--- generate_badges.py
+++ generate_badges.py
@@ -1,40 +1,51 @@
 import anybadge
 import json
+import xml.etree.ElementTree as ET
+from pylint.lint import Run
+
 
 def create_badge(value, label, filename, thresholds):
     badge = anybadge.Badge(label, value, thresholds=thresholds)
     badge.write_badge(filename)
 
-def get_pylint_score(pylint_report_file):
-    with open(pylint_report_file, 'r') as file:
-        pylint_data = json.load(file)
 
-    if not pylint_data:
-        print('Error: pylint-report.json is empty or has an unexpected format.')
+def get_pylint_score(pylint_target_file):
+    results = Run([pylint_target_file], do_exit=False)
+    return results.linter.stats.global_note
+
+
+def get_test_coverage(coverage_file):
+    tree = ET.parse(coverage_file)
+    root = tree.getroot()
+    coverage_percentage = 0
+
+    try:
+        lines_total = int(root.get('lines-valid'))
+        lines_covered = int(root.get('lines-covered'))
+        coverage_percentage = (lines_covered / lines_total) * 100
+    except (ValueError, TypeError):
+        print("Error: coverage.xml is empty or has an unexpected format.")
         return 0
 
-    score = pylint_data[0]['score']
-    return score
+    return round(coverage_percentage,2)
 
 
-def get_test_coverage(filename):
-    with open(filename, 'r') as f:
-        for line in f:
-            if 'TOTAL' in line:
-                coverage_line = line
-                break
-    coverage_percentage = float(coverage_line.split()[-1].strip('%'))
-    return coverage_percentage
-
-def get_pep8_score(filename):
-    with open(filename, 'r') as f:
-        total_errors = sum(int(line.split()[0]) for line in f)
+def get_pep8_score(flake8_report_file):
+    with open(flake8_report_file, 'r') as f:
+        lines = f.readlines()
+        last_line = lines[-1]
+        total_errors = int(last_line.strip())
     return total_errors
 
+
 def main():
-    pylint_score = get_pylint_score('pylint-report.json')
+
+    pylint_score = get_pylint_score('app.py')
+    print(pylint_score)
     test_coverage = get_test_coverage('coverage.xml')
+    print(test_coverage)
     pep8_compliance = get_pep8_score('flake8-report.txt')
+    print(pep8_compliance)
 
     code_quality_thresholds = {50: 'red', 70: 'orange', 85: 'yellow', 95: 'green'}
     test_coverage_thresholds = {50: 'red', 70: 'orange', 85: 'yellow', 95: 'green'}
